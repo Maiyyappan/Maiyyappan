47159c15aea6f89196a84ee710d51a326dd456cb
Maiyyappan <46838562+Maiyyappan@users.noreply.github.com>
2019-01-29 14:50:18 +0530

    Create 5muliples.c

---
+++ 5muliples.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int a,mul,i;
+    scanf("%d",&a);
+    for(i=1;i<=5;i++)
+    { 
+        mul=a*i;
+        printf("%d ",mul);
+    }
+    return 0;
+}

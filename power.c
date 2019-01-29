13ba1544be9af3917175ec669accc044680924e2
Maiyyappan <46838562+Maiyyappan@users.noreply.github.com>
2019-01-29 14:09:27 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+ int a,i,k,sum=0;
+ scanf("%d",&a);
+ scanf("%d",&k);
+ for(i=0;i<k;i++)
+ {
+     sum=a*a;
+ }
+ printf("%d",sum);
+    return 0;
+}

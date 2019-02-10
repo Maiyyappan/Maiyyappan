19e611b5008ebc0f692b7aaaa07abf5fae280b6c
Maiyyappan <46838562+Maiyyappan@users.noreply.github.com>
2019-02-10 07:09:34 -0800

    Update 23.c

--- 23.c
+++ 23.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
+#include<conio.h>
 void main()
 {
 int n,temp,i,j,a[20];
-printf("enter the array of numbers");
 scanf("%d",&n);
-printf("enter the numbers");
 for(i=0;i<=n;i++)
 {
 scanf("%d",&a[i]);

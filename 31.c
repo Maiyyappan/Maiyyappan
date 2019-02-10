7564c092ae5f2faea5fd0b0eeb853c3750465a55
Maiyyappan <46838562+Maiyyappan@users.noreply.github.com>
2019-02-10 07:27:09 -0800

    Update 31.c

--- 31.c
+++ 31.c
@@ -2,14 +2,14 @@
 #include<string.h>
 void main()
 {
-char str[50],i,c=0;
+char str[50],i,count=0;
 gets(str);
 for(i=0;str[i]!='\0';i++)
 {
 if(str[i]>'a' &&str[i]<'z')
 {
-c++;
+count++;
 }
 }
-printf("%d",c);
+printf("%d",count);
 }

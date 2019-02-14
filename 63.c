fdf58b760951836bb9180a1295b8b4187f92e382
Maiyyappan <46838562+Maiyyappan@users.noreply.github.com>
2019-02-14 16:11:07 +0530

    Create 63.c

---
+++ 63.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a[10],i,c=0;
+clrscr();
+for(i=0;i<10;i++)
+{
+scanf("%d",&a[i]);
+}
+c=a[0];
+for(i=1;i<10;i++)
+{
+if(a[i]<k)
+c=a[i];
+}
+printf("%d",c);
+}

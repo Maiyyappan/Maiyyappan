c624119e9228f0d795a0339d57252c14cdc577ab
Maiyyappan <46838562+Maiyyappan@users.noreply.github.com>
2019-02-14 15:59:08 +0530

    Create 52.c

---
+++ 52.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+int N;
+clrscr();
+scanf("%d",&N);
+if(N==1)
+printf("one");
+else if(N==2)
+printf("two");
+else if(N==3)
+printf("three");
+else if(N==4)
+printf("four");
+else if(N==5)
+printf("five");
+else if(N==6)
+printf("six");
+else if(N==7)
+printf("seven");
+else if(N==8)
+printf("eight");
+else if(N==9)
+printf("nine");
+else if(N==10)
+printf("ten");
+else
+printf("invalid");
+getch();
+}

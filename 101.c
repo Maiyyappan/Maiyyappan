2dbfc60cf130f39d289ffdd00aa9530c61499b6e
Maiyyappan <46838562+Maiyyappan@users.noreply.github.com>
2019-02-14 22:18:59 +0530

    Create 101.c

---
+++ 101.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+char a[10];
+int n,i,m,1l=0;
+clrscr();
+scanf("%s",&a);
+scanf("%d",&n);
+for(i=0;a[i]!='\0';i++)
+{
+l++;
+}
+m=l1-n;
+for(i=m;i<l1;i++)
+{
+printf("%c",a[i]);
+}
+}

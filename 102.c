fa12e2084dec3dfae82db5652000a0af33015f8c
Maiyyappan <46838562+Maiyyappan@users.noreply.github.com>
2019-02-14 22:20:09 +0530

    Create 102.c

---
+++ 102.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int n1;
+    scanf("%d",&n1);
+    if(n1%2==0)
+    {
+        n=n1/2;
+        printf("%d",n);
+    }
+    else
+    {
+        printf("%d",n1);
+    }
+}

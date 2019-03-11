65540579615070f02eb9f317191a50197af87ede
Maiyyappan <46838562+Maiyyappan@users.noreply.github.com>
2019-03-11 22:50:21 +0530

    Create set2_39.c

---
+++ set2_39.c
@@ -0,0 +1,20 @@
+    
+#include <stdio.h>
+
+int main()
+{
+   int n,f=0,i;
+   scanf("%d",&n);
+   for(i=0;i<=i;i++)
+   {
+       if(i*i==n)
+       {
+           f=1;break;
+       }
+   }
+   if(f==1)
+   printf("yes");
+   else
+   printf("no");
+
+}

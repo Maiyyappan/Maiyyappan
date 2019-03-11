126c6aabd7b7ceb4c8975e0c35fda33361eba87b
Maiyyappan <46838562+Maiyyappan@users.noreply.github.com>
2019-03-11 22:30:19 +0530

    Create set2_66.c

---
+++ set2_66.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,f=0;
+clrscr();
+for(i=2;i<=/2;i++)
+{
+if(n%i==0)
+{
+f=1;
+break;
+}
+}
+if(f==0)
+printf("prime no");
+else 
+printf("no");
+}

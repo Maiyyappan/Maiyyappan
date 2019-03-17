393a9c667a4429e874c0b1a4d220bfc2eddd4735
Maiyyappan <46838562+Maiyyappan@users.noreply.github.com>
2019-03-17 21:36:18 +0530

    Create set2_86.c

---
+++ set2_86.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() 
+{
+	int n;
+	scanf("%d",&n);
+	int array[n],i,res=0;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d ",&array[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		res=res^array[i];
+	}
+	printf("%d",res);
+}

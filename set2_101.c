f1e92825e3428d84a103e7e6e824bfd8b8c6b4f3
Maiyyappan <46838562+Maiyyappan@users.noreply.github.com>
2019-03-19 15:33:44 +0530

    Create set2_101.c

---
+++ set2_101.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i,a[100],sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+     scanf("%d",&a[i]);   
+    }
+    for(i=1;i<n;i++)
+    {
+        sum=sum+a[i];
+    }
+    printf("%d",sum);
+
+    return 0;
+}

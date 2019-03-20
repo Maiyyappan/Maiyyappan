74ad9addcf94b7c4934f7b1dcbf2c85e4efa0c51
Maiyyappan <46838562+Maiyyappan@users.noreply.github.com>
2019-03-20 11:45:37 +0530

    Create set2_108.c

---
+++ set2_108.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<conio.h>
+void main() 
+{
+int a[100],n,i,j,s=0;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+  scanf("%d",&a[i]);
+
+}
+for(i=0;i<n;i++)
+{
+  s=s+a[i];
+  printf("%d",c);
+}
+getch();
+}

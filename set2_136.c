fba5733a13eca9e029b7af33bed58338f19ca03c
Maiyyappan <46838562+Maiyyappan@users.noreply.github.com>
2019-03-22 11:48:10 +0530

    Create set2_136.c

---
+++ set2_136.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,a[100],i,j,c=0,k,m=0,t,b[100];
+    scanf("%d%d",&n,&k);
+    for(i=0;i<n;i++)
+            scanf("%d",&a[i]);
+    for(i=0;i<n;i++)
+    {
+        c=1;
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                a[j]='$';
+                c++;
+            }
+        }
+            if(c>=k)
+            {
+            a[i]='$';
+            }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]!='$')
+        {
+            b[m]=a[i];
+            m++;
+        }
+        }
+    for(i=0;i<m;i++)
+    {
+        for(j=i+1;j<m;j++)
+        {
+            if(b[i]>b[j])
+            {
+                t=b[i];
+                b[i]=b[j];
+                b[j]=t;
+            }
+        }
+        printf("%d ",b[i]);
+    }getch();
+  }

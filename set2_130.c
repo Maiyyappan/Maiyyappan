b2df58ea2738ea07030763c189c2076e23351ae3
Maiyyappan <46838562+Maiyyappan@users.noreply.github.com>
2019-03-22 11:43:53 +0530

    Create set2_130.c

---
+++ set2_130.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,n1,rem,bin,count=0,power=1;
+scanf("%d",&n);
+n1=n;
+while(n1>0)
+{
+    rem=n1%2;
+    bin=bin+rem*power;
+    n1=n1/2;
+    count++;
+    power=power*10;
+}
+if(n==0)
+printf("1");
+else
+printf("%d",count);
+getch();
+}

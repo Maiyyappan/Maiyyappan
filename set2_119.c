a06efcd832659e2898b641892693899c1acc9966
Maiyyappan <46838562+Maiyyappan@users.noreply.github.com>
2019-03-22 11:14:27 +0530

    Create set2_119.c

---
+++ set2_119.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<conio.h>
+void main() 
+{
+	char str[30];
+  clrscr();
+	scanf("%s",str);
+	int i,len,value,sum=0;
+	len=strlen(str);
+	int j=len-1;
+	for(i=0;str[i]!='\0';i++)
+	{
+  
+		if(str[i]>='A' && str[i]<='F')
+		value=(str[i]-65)+10;
+	
+		else if(str[i]>='0' && str[i]<='9')
+			value=str[i]-48;
+		sum=sum+(value*(pow(16,j)));
+		j--;
+	}
+	printf("%d",sum);
+  getch();
+}

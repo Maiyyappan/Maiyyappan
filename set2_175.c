eb6febd93bd24eefd5cb1314ebe6f10a6d7b7b60
Maiyyappan <46838562+Maiyyappan@users.noreply.github.com>
2019-03-24 17:13:45 +0530

    Create set2_175.c

---
+++ set2_175.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+    int a[100],i,n,k,f=0,c;
+
+    
+    scanf("%d %d",&n,&k);
+    for(i=0;i<n;i++)
+       scanf("%d",&a[i]);
+   
+    c=a[0];
+    for(i=0;i<n;i++)
+    {
+    
+         if((a[i]>c)&&(a[i]>k))
+         {
+             printf("%d",a[i]);
+             break;
+         }
+         else
+           c=a[i];    
+    }
+
+}

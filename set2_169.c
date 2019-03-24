11a85b40ecfbccb8adfc3995fb0ad6a7ae3d75ee
Maiyyappan <46838562+Maiyyappan@users.noreply.github.com>
2019-03-24 17:09:00 +0530

    Create set2_169.c

---
+++ set2_169.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+void main()
+{
+    int n1,n2,rem,rem1,pow=1,pow1=1,product,rem2,c=0,bin=0,bin1=0;
+    scanf("%d %d",&n1,&n2);
+    while(n1)
+    {
+        rem=n1%2;
+        bin=bin+rem*pow;
+        n1=n1/2;
+        pow=pow*10;
+    }
+    while(n2)
+    {
+        rem1=n2%2;
+        bin1=bin1+rem1*pow1;
+        n2=n2/2;
+        pow1=pow1*10;
+    }
+    product=bin*bin1;
+while(product)
+{
+    rem2=product%10;
+    product=product/10;
+    if(rem2==1)
+       c++;   
+}
+printf("%d",c);
+ return 0;}

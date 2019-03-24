9ca0c6a01121e8bf81a25b627a0fabbc2dedb12e
Maiyyappan <46838562+Maiyyappan@users.noreply.github.com>
2019-03-24 18:51:50 +0530

    Rename set2_99.c to set2_199.c

---
+++ set2_199.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+    
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if((a!=b)&&(b!=c)&&(c!=a))
+    printf("yes");
+    else
+    {
+    printf("no"); 
+}}
